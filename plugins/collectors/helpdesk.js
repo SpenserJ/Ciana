4967e9f1ab1c2a7aed0e3a775ee909d35b15494b
Spenser Jones <spenser.e.jones@gmail.com>
2013-01-10 23:56:28 +0000

    Adding support for running collector plugins
    Collector plugins will retrieve data from an external source at a
    given interval.

---
+++ plugins/collectors/helpdesk.js
@@ -0,0 +1,13 @@
+var Plugin_Helpdesk = function() {
+  
+}
+
+Plugin_Helpdesk.prototype.get = function() {
+  return "This be the helpdesk.Get";
+}
+
+Plugin_Helpdesk.prototype.tickets = function() {
+  return "This be the helpdesk.tickets";
+}
+
+module.exports = Plugin_Helpdesk;
---
+++ plugins/collectors/time.js
@@ -0,0 +1,9 @@
+var Plugin_Time = function() {
+  
+}
+
+Plugin_Time.prototype.get = function() {
+  return new Date();
+}
+
+module.exports = Plugin_Time;
