774f858baa71b20e6acb536dd6b5fb070bb65eb2
Spenser Jones <spenser.e.jones@gmail.com>
2013-01-11 23:43:16 +0000

    Updating appearance of panels

--- public/scripts/ciana.js
+++ public/scripts/ciana.js
@@ -1,7 +1,7 @@
 var socket = io.connect();
 socket.on('provider_update', function (data) {
   var $panel = $('#' + data.name);
-  $panel.html(data.data);
+  $('.content', $panel).html(data.data);
   console.log('provider_update', data);
 });
 socket.on('initialize', function (data) {
