64f7a279de00ccb1ae4cd826c8999e72a63cace7
Spenser Jones <spenser.e.jones@gmail.com>
2013-05-30 21:24:55 -0600

    Converting Collector base and Time providers to use PseudoClass

--- providers/time.js
+++ providers/time.js
@@ -1,23 +1,23 @@
-var Provider_Time = function Provider_Time(config) { this.configure(config); };
-Provider_Time.prototype = new (require('../lib/provider/collector'));
-Provider_Time.prototype.name = 'time';
-Provider_Time.prototype.defaults = {
-  frequency: 1
-};
+var Collector = require('../lib/provider/collector');
 
-Provider_Time.prototype.tick = function() {
-  this.emit({ date: new Date() });
-};
+var Provider_Time = Collector.extend({
+  toString: 'Provider_Time',
+  name: 'time',
+  frequency: 1,
 
-var toTemplate = {
-  toString: function toTime(data) {
-    var date = new Date(data.date);
-    return { text: date.toLocaleDateString() + ' at ' + date.toLocaleTimeString() };
+  tick: function tick() {
+    this.emit({ date: new Date()  });
   },
-  text_date: function text_date(data) {
-    return { text: new Date(data.date).toLocaleDateString() };
-  }
-};
 
-module.exports.provider = Provider_Time;
-module.exports.toTemplate = toTemplate;
+  toTemplate: {
+    toString: function toString(data) {
+      var date = new Date(data.date);
+      return { text: date.toLocaleDateString() + ' at ' + date.toLocaleTimeString() };
+    },
+    text_date: function text_date(data) {
+      return { text: new Date(data.date).toLocaleDateString() };
+    }
+  }
+});
+
+module.exports = Provider_Time;
