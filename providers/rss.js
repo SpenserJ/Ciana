8d9de64c2357320aa3816f4e2f2da578d4eaaaa5
Spenser Jones <spenser.e.jones@gmail.com>
2013-05-30 21:32:05 -0600

    Converting RSS provider to use PseudoClass

--- providers/rss.js
+++ providers/rss.js
@@ -1,38 +1,36 @@
-var _ = require('lodash')._
+var Collector = require('../lib/provider/collector')
   , parser = require('feedparser');
 
-var Provider_RSS = function Provider_RSS(config) { this.configure(config); };
-Provider_RSS.prototype = new (require('../lib/provider/collector'));
-Provider_RSS.prototype.name = 'rss';
-Provider_RSS.prototype.defaults = {
-  frequency: 5 * 60
-};
+var Provider_RSS = Collector.extend({
+  toString: 'Provider_RSS',
+  name: 'rss',
+  frequency: 5 * 60,
 
-Provider_RSS.prototype.tick = function() {
-  var self = this;
-  parser.parseUrl(self.url, { addmeta: false }, function(error, meta, articles) {
-    self.emit({ error: error, meta: meta, articles: articles });
-  });
-};
-
-var toTemplate = {
-  toString: function toString(data) {
-    console.log(data);
-    var value = '';
-    $.each(data.articles, function(index, article) {
-      value += article.title + "\n";
+  tick: function tick() {
+    var self = this;
+    parser.parseUrl(self.url, { addmeta: false }, function (error, meta, articles) {
+      self.emit({ error: error, meta: meta, articles: articles });
     });
-    return { text: value };
   },
-  html: function html(data) {
-    var value = '<ul>';
-    $.each(data.articles, function(index, article) {
-      value += '<li><a href="' + article.link + '" target="_blank">' + article.title + '</a></li>';
-    });
-    value += '</ul>';
-    return { html: value };
-  }
-};
 
-module.exports.provider = Provider_RSS;
-module.exports.toTemplate = toTemplate;
+  toTemplate: {
+    toString: function toString(data) {
+      console.log(data);
+      var value = '';
+      $.each(data.articles, function(index, article) {
+        value += article.title + "\n";
+      });
+      return { text: value };
+    },
+    html: function html(data) {
+      var value = '<ul>';
+      $.each(data.articles, function(index, article) {
+        value += '<li><a href="' + article.link + '" target="_blank">' + article.title + '</a></li>';
+      });
+      value += '</ul>';
+      return { html: value };
+    }
+  }
+});
+
+module.exports = Provider_RSS;
